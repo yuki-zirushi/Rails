be89de8ad4efbb34f69399ed84b16092061eb941
yuki-zirushi <yuko.12.151@gmail.com>
2021-04-16 05:07:12 +0000

    [Add] image

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -38,7 +38,7 @@ class TodolistsController < ApplicationController
 
   def list_params
 
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
 
   end
 
