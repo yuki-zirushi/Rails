d5021e5f84f384b8dc570c5e18a2138f477060fd
yuki-zirushi <yuko.12.151@gmail.com>
2021-04-16 05:24:11 +0000

    [Add] secret_key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'a39b3de7042f432d86d8e600f99db15404499e50cc620a0f91a69b8f3571f4c22265f4fbebdcbdb437999024b002fc632ae5e1a7253d517b12a669603255ca43'
